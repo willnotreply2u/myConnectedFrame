8d1c63f85a55e06f32e8841de119757102544470
willnotreply2u <32102324+willnotreply2u@users.noreply.github.com>
2017-09-19 09:56:32 -0500

    Update start.sh
    added resin-wifi-connect

--- start.sh
+++ start.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Start resin-wifi-connect
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+sleep 1
+node resin-wifi-connect/src/app.js --clear=false
+
+# At this point the WiFi connection has been configured and the device has
+# internet - unless the configured WiFi connection is no longer available.
+
+# By default docker gives us 64MB of shared memory size but to display heavy
+# pages we need more.
+umount /dev/shm && mount -t tmpfs shm /dev/shm
+
+# using local electron module instead of the global electron lets you
+# easily control specific version dependency between your app and electron itself.
+# the syntax below starts an X istance with ONLY our electronJS fired up,
+# it saves you a LOT of resources avoiding full-desktops envs
+
 # By default docker gives us 64MB of shared memory size but to display heavy
 # pages we need more.
 umount /dev/shm && mount -t tmpfs shm /dev/shm
